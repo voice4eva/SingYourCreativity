d6cd74f3c3ec937e0f6e726669b77c307d2e0c99
Sylvester Klirowski <Sly@Sylvesters-MacBook-Pro.local>
2016-05-18 23:14:16 +0200

    stop videos upon exit

--- js/modal.js
+++ js/modal.js
@@ -7,20 +7,30 @@ var span = document.getElementsByClassName("modal-close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
+
+    // reset the iframe src to stop the playing video
+    $("#ytVideoTAL").attr('src','https://www.youtube.com/embed/Ii19TxBraQM');
+    $("#ytVideoObjectives").attr('src','https://www.youtube.com/embed/sibICXCrtsM');
+
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+
+        // reset the iframe src to stop the playing video
+        $("#ytVideoTAL").attr('src','https://www.youtube.com/embed/Ii19TxBraQM');
+        $("#ytVideoObjectives").attr('src','https://www.youtube.com/embed/sibICXCrtsM');
     }
+
 }
 
 // switch modal videos
 function switchVideos() {
   // hide all videos
   $("div[id*='modalVideo']").addClass('hidden');
-  
+
   // set modal style of display
   modal.style.display = "block";
 
