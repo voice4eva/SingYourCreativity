6a71086d468342bea5b4920bbc0545b133db4254
Sylvester Klirowski <Sly@Sylvesters-MacBook-Pro.local>
2016-06-08 16:38:18 -0400

    fixed form-process php link

--- js/form-scripts.js
+++ js/form-scripts.js
@@ -19,7 +19,7 @@ function submitForm(){
 
     $.ajax({
         type: "POST",
-        url: "php/form-process.php",
+        url: "../php/form-process.php",
         data: "name=" + name + "&email=" + email + "&message=" + message,
         success : function(text){
             if (text == "success"){
