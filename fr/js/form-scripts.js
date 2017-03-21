9aa2f142913fce1e715dcd68df875ebcd2df49ba
Sylvester Klirowski <Sly@Sylvesters-MacBook-Pro.local>
2017-03-20 22:52:08 -0400

    added date picker, functions

--- fr/js/form-scripts.js
+++ fr/js/form-scripts.js
@@ -21,7 +21,7 @@ function submitForm(){
     $.ajax({
         type: "POST",
         url: "../php/form-process.php",
-        data: "name=" + name + "date=" + date + "&email=" + email + "&message=" + message,
+        data: "name=" + name + "&date=" + date + "&email=" + email + "&message=" + message,
         success : function(text){
             if (text == "success"){
                 formSuccess();
