372f81ef1a052d0fce1e3b0a46a3abfe51c12860
Sylvester Klirowski <Sly@Sylvesters-MBP.fritz.box>
2017-05-01 22:36:37 +0200

    added confirmation function to form-scripts and php file

--- js/form-scripts.js
+++ js/form-scripts.js
@@ -1,85 +1,91 @@
 (function($) {
-  $("#contactForm").validator().on("submit", function (event) {
-    if (event.isDefaultPrevented()) {
-        // handle the invalid form...
-        formError();
-        submitMSG(false, "Did you fill in the form properly?");
-    } else {
-        // everything looks good!
-        event.preventDefault();
-        submitForm();
-    }
-});
+    // validate form entries
+    $("#contactForm").validator().on("submit", function(event) {
+        if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+            formError();
+            submitMSG(false, "Did you fill in the form properly?");
+        } else {
+            // everything looks good!
+            event.preventDefault();
+            submitForm();
+        }
+    });
 
 
-function submitForm(){
-    // Initiate Variables With Form Content
-    var name = $("#name").val();
-    var date = $("#datepicker").val();
-    var email = $("#email").val();
-    var message = $("#message").val();
+    function submitForm() {
+        // Initiate Variables With Form Content
+        var name = $("#name").val();
+        var date = $("#datepicker").val();
+        var email = $("#email").val();
+        var message = $("#message").val();
 
-    $.ajax({
-        type: "POST",
-        url: "../php/form-process.php",
-        data: "name=" + name + "&date=" + date + "&email=" + email + "&message=" + message,
-        success : function(text){
-            if (text == "success"){
-                formSuccess();
-            } else {
-                formError();
-                submitMSG(false,text);
+        $.ajax({
+            type: "POST",
+            url: "../php/form-process.php",
+            data: "name=" + name + "&date=" + date + "&email=" + email + "&message=" + message,
+            success: function(text) {
+                if (text == "success") {
+                    formSuccess();
+                    sendConfEmail(name, date, email, message);
+                } else {
+                    formError();
+                    submitMSG(false, text);
+                }
             }
-        }
-    });
-}
-
-function formSuccess(){
-    $("#contactForm")[0].reset();
-    submitMSG(true, "Your message has been submitted! Thank you. We will be touch shortly.")
-}
-
-function formError(){
-    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-        $(this).removeClass();
-    });
-}
-
-function submitMSG(valid, msg){
-    if(valid){
-        var msgClasses = "alert alert-success text-center tada animated col-sm-10 col-sm-offset-2";
-    } else {
-        var msgClasses = "alert alert-danger text-center tada animated col-sm-10 col-sm-offset-2";
+        });
     }
-    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
-}
 
-/* Datepicker */
-$( function() {
-  // Set only wednesdays to be selectable
-  // $("#datepicker").datepicker({beforeShowDay: function(date){
-  //         return [date.getDay() == 3, ''];
-  // }});
-  var daysToDisable = [0, 2, 4, 5, 6, 7];
-
-  $("#datepicker").datepicker({beforeShowDay: function(date){
-    var day = date.getDay();
-    for (i = 0; i < daysToDisable.length; i++) {
-        if ($.inArray(day, daysToDisable) != -1) {
-            return [false];
-        }
+    function formSuccess() {
+        $("#contactForm")[0].reset();
+        submitMSG(true, "Your message has been submitted! Thank you. We will be touch shortly.")
     }
-    return [true];
-  }});
 
-  // show datapicker per list selection
-  // $( "select" ).change(function() {
-  //   if ($(this).val() == "SingWellBeing_Weds"){
-  //     $("#dateDiv").removeClass("hidden");
-  //   }
-  //   else{
-  //     $("#dateDiv").addClass("hidden");
-  //   }
-  // });
-});
- })(jQuery);
+    function formError() {
+        $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $(this).removeClass();
+        });
+    }
+
+    function submitMSG(valid, msg) {
+        if (valid) {
+            var msgClasses = "alert alert-success text-center tada animated col-sm-10 col-sm-offset-2";
+        } else {
+            var msgClasses = "alert alert-danger text-center tada animated col-sm-10 col-sm-offset-2";
+        }
+        $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+    }
+
+    function sendConfEmail(name, date, email, message){
+      $.ajax({
+          type: "POST",
+          url: "../php/conf-email.php",
+          data: "name=" + name + "&date=" + date + "&email=" + email + "&message=" + message,
+          success: function(text) {
+              if (text == "pass") {
+                  console.log("email send succeeded");
+              } else {
+                  console.log("email send failed");
+              }
+          }
+      });
+    }
+
+    /* Datepicker */
+    $(function() {
+
+        var daysToDisable = [0, 2, 4, 5, 6, 7];
+
+        $("#datepicker").datepicker({
+            beforeShowDay: function(date) {
+                var day = date.getDay();
+                for (i = 0; i < daysToDisable.length; i++) {
+                    if ($.inArray(day, daysToDisable) != -1) {
+                        return [false];
+                    }
+                }
+                return [true];
+            }
+        });
+    });
+})(jQuery);
---
+++ php/conf-email.php
@@ -0,0 +1,44 @@
+<?php
+
+ $fromEMAIL = "northeasternhusky@yahoo.com";
+
+ // fields
+ $name = $_POST["name"];
+ $date = $_POST["date"];
+ $email = $_POST["email"];
+ $phone = $_POST["phone"];
+ $message = $_POST["message"];
+
+ $EmailTo = "klirowski.s@gmail.com";
+ $Subject = "Confirmation Email";
+
+ // prepare email body text
+ $Body = "<html><body><h1>Confirmation Email</h1><p>Dear ";
+ $Body .= $name;
+ $Body .= "</br>Thank you for registering for SYC. This is confirmation for the following date: ";
+ $Body .= $date;
+ $Body .= "</br>";
+ $Body .= "Your info and message:</br>Email: ";
+ $Body .= $email;
+ $Body .= "</br>";
+ $Body .= "Phone: ";
+ $Body .= $phone;
+ $Body .= "</br>";
+ $Body .= "Message: ";
+ $Body .= $message;
+ $Body .= "</p></body></html>";
+
+ // send email
+ $success = mail($EmailTo, $Subject, $Body, "From:".$fromEMAIL);
+
+ // redirect to success page
+ if ($success && $errorMSG == ""){
+    echo "pass";
+ }else{
+     if($errorMSG == ""){
+         echo "fail";
+     } else {
+         echo $errorMSG;
+     }
+ }
+?>
